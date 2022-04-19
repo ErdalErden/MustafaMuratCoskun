1e3bf182aeb7657312a2a5b19fd2af23c54b2dec
ErdalErden <you@example.com>
2022-04-19 23:46:40 +0300

    üzerinde çalışıldı

---
+++ ReactJsDersleri/reactapp/src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <ul className="navList">
+        <li className="navListItem">
+          <a href="/" className="navListLink">Home</a>
+        </li>
+        <li className="navListItem">
+          <a href="/" className="navListLink">Products</a>
+        </li>
+        <li className="navListItem">
+          <a href="/" className="navListLink">Services</a>
+        </li>
+        <li className="navListItem">
+          <a href="/" className="navListLink">About</a>
+        </li>
+        <li className="navListItem">
+          <a href="/" className="navListLink">Contact</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
