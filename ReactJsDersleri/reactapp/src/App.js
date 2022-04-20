c268b225d440b1425fc23e788357b54a5d0e904c
ErdalErden <you@example.com>
2022-04-20 23:26:44 +0300

    projeye bootstrap eklendi

--- ReactJsDersleri/reactapp/src/App.js
+++ ReactJsDersleri/reactapp/src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
-import Header from './components/header/Header'
-import Navbar from './components/navbar/Navbar'
 import User from './components/user/User'
 
 class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <Header/>
-        <Navbar/> 
-        <h4>App Componenti</h4>
+      <div className='container'>
+        <h4 className='title'>App Componenti</h4>
         <User/>  
       </div>
     )
