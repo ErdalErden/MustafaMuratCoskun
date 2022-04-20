944eb3172089b6187f6a3ad748676f6cce5a16f1
ErdalErden <you@example.com>
2022-04-20 23:13:57 +0300

    ilk komponenti oluşturma işlendi

--- ReactJsDersleri/reactapp/src/App.js
+++ ReactJsDersleri/reactapp/src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Navbar from './components/navbar/Navbar'
-
+import User from './components/user/User'
 
 class App extends Component {
   render() {
     return (
       <div className='app'>
         <Header/>
-        <Navbar/>
+        <Navbar/> 
+        <h4>App Componenti</h4>
+        <User/>  
       </div>
     )
   }
