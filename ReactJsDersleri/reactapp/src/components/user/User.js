b34d2987806d8ab58bc0160ab0aa5f5310529013
ErdalErden <you@example.com>
2022-04-21 23:08:55 +0300

    Fontawesome ve bootstarp kullanımı

--- ReactJsDersleri/reactapp/src/components/user/User.js
+++ ReactJsDersleri/reactapp/src/components/user/User.js
@@ -8,12 +8,19 @@ class User extends Component {
     //Destructing
     const {name,department,salary } = this.props;
     return (
-      <div className='user'>
-        <ul className='userList'>
-          <li className='userListTitle'>Name: {name}</li>
-          <li>Departman: {department}</li>
-          <li>Salary: {salary}</li>
-        </ul>
+      <div className='col-md-8 mb-4'>
+        <div className="card">
+          <div className="card-header d-flex justify-content-between">
+            <h4 className="d-inline">
+              {name}
+            </h4>
+            <i className="far fa-trash-alt" style={{cursor:"pointer"}}></i>
+          </div>
+          <div className="card-body">
+            <p className="card-text">Maaş:{salary}</p>
+            <p className="card-text">Departman:{department}</p>
+          </div>
+        </div>
       </div>
     )
   }
